54895a634690b8058b2234c5695543796252d98c
shrimpMD <dr.shrimp@aol.com>
2021-03-09 14:34:20 +0400

    video mobile ready

--- resourses/js/videobox.js
+++ resourses/js/videobox.js
@@ -1,9 +1,82 @@
+
+
+
+//Rain & Dust
 function openRainDust() {
     document.getElementById("raindust").style.display = "block";
     document.getElementById("raindust-fr").innerHTML = `<iframe width="560" height="315" src='https://www.youtube.com/embed/eVa1CVopRD8' frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
   }
-
   function closeRainDust() {
     document.getElementById("raindust").style.display = "none";
     document.getElementById("raindust-fr").innerHTML = "";
   }
+
+  //Medley
+function openMedley() {
+  document.getElementById("medley").style.display = "block";
+  document.getElementById("medley-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/WnxHmgKAhbg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeMedley() {
+  document.getElementById("medley").style.display = "none";
+  document.getElementById("medley-fr").innerHTML = "";
+}
+
+ //Diamonds & Glass
+ function openDG() {
+  document.getElementById("dg").style.display = "block";
+  document.getElementById("dg-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/Vg9oFAd8_ec" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeDG() {
+  document.getElementById("dg").style.display = "none";
+  document.getElementById("dg-fr").innerHTML = "";
+}
+
+//Apologise & Promise
+function openAP() {
+  document.getElementById("ap").style.display = "block";
+  document.getElementById("ap-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/C_7bgnPWJNI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeAP() {
+  document.getElementById("ap").style.display = "none";
+  document.getElementById("ap-fr").innerHTML = "";
+}
+
+//Razzberty
+function openRazz() {
+  document.getElementById("razz").style.display = "block";
+  document.getElementById("razz-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/j_jWbETTA1c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeRazz() {
+  document.getElementById("razz").style.display = "none";
+  document.getElementById("razz-fr").innerHTML = "";
+}
+
+//Sur Le Fil
+function openSlf() {
+  document.getElementById("slf").style.display = "block";
+  document.getElementById("slf-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/LkWgYXfndpI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeSlf() {
+  document.getElementById("slf").style.display = "none";
+  document.getElementById("slf-fr").innerHTML = "";
+}
+
+//Doshechka
+function openVD() {
+  document.getElementById("vd").style.display = "block";
+  document.getElementById("vd-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/HKTlkM2QqyI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeVD() {
+  document.getElementById("vd").style.display = "none";
+  document.getElementById("vd-fr").innerHTML = "";
+}
+
+//Lugsana
+function openLugsana() {
+  document.getElementById("lugsana").style.display = "block";
+  document.getElementById("lugsana-fr").innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/MvdHkoRa8P0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+}
+function closeLugsana() {
+  document.getElementById("lugsana").style.display = "none";
+  document.getElementById("lugsana-fr").innerHTML = "";
+}
